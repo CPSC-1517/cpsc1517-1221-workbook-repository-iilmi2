18ecdb79bc807e1fb00e81e7b1f082c96529312a
Idris Ilmi <iilmi2@nait.ca>
2022-09-06 14:51:25 -0600

    End of class

---
+++ CPSC1517-1221-A03-demos/OOPReview1/HockeyTeam.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReview1
+{
+    public class HockeyTeam
+    {
+        public enum Conferences
+        {
+            West,
+            East
+        }
+        public enum Divisions
+        {
+            Metro,
+            Atlantic,
+            Central,
+            Pacific
+        }
+        private string name;
+        private Conferences _conference;
+        private Divisions _division;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Conferences Conference
+        {
+            get { return _conference; }
+        }
+        public Divisions Division
+        {
+            get { return _division; }
+        }
+    }
+}
