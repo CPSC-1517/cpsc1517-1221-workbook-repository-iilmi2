ab0328ea20e3a6c96347eb2fd6be2e1f6fd300b4
Idris Ilmi <iilmi2@nait.ca>
2022-09-07 16:54:05 -0600

    We can add teams and calc points now.

--- CPSC1517-1221-A03-demos/OOPReview1/Program.cs
+++ CPSC1517-1221-A03-demos/OOPReview1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Idris is the coolest");
+using OOPReview1;
+
+NHL_Team Senator = new NHL_Team(NHLConference.East, NHLDivision.Atlantic, "Senators", "Ottawa");
+Senator.GamesPlayed = 82;
+Senator.Wins = 33;
+Senator.Losses = 42;
+Senator.OvertimeLossses = 7;
+Console.WriteLine(Senator.Points);
