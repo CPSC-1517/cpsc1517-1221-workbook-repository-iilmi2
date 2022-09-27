e29c6faaff1861115904d0829eb711898df57e94
iilmi2 <iilmi2@nait.ca>
2022-09-27 14:34:39 -0600

    There are missing methods and json serializer to read is not working

--- NHLSystem/NHLSystem/NHLSystem/Player.cs
+++ NHLSystem/NHLSystem/NHLSystem/Player.cs
@@ -30,7 +30,7 @@ namespace NHLSystem
         {
             get { return Goals + Assists; }
         }
-        public Player(string fullName, Position pos, int primNo, int goals, int assists) : base(fullName)
+        public Player(string fullName, int primNo, Position pos, int goals, int assists) : base(fullName)
         {
             Position = pos;
             Goals = goals;
@@ -47,6 +47,23 @@ namespace NHLSystem
         public override string ToString()
         {
             return $"{FullName}, {PrimaryNo}, {Position}, {Goals}, {Assists}, {Points}";
+        }
+
+        public static Player ParseCsv(string line)
+        {
+            const char Delimiter = ',';
+            string[] tokens = line.Split(Delimiter);
+            if(tokens.Length != 6)
+            {
+                throw new FormatException($"CSV line must contain exactly 6 values. {line}");
+            }
+            string playerName = tokens[0];
+            int playerNumber = int.Parse(tokens[1]);
+            Position position = (Position) Enum.Parse(typeof(Position), tokens[2]);
+            int goals = int.Parse(tokens[3]);
+            int assists = int.Parse(tokens[4]);
+            return new Player(playerName, playerNumber, position, goals, assists);
+
         }
     }
 }
--- NHLSystem/NHLSystem/NHLSystem/Program.cs
+++ NHLSystem/NHLSystem/NHLSystem/Program.cs
@@ -8,6 +8,8 @@
  * 5- full name < 3 characters
  * */
 using NHLSystem;
+using System.Collections.Generic;
+using System.Text.Json;
 
 Person validPerson = new Person("Connor McDavid");
 Console.WriteLine(validPerson.FullName);
@@ -44,3 +46,122 @@ foreach(Player player in oilersTeam.Players)
     Console.WriteLine(player);
 }
 Console.WriteLine($"Total points is {oilersTeam.TotalPlayerPoints} ");
+CreateTeamJsonFile();
+oilersTeam = ReadTeamInfoFromJsonFile();
+
+DisplayTeamInfo(oilersTeam);
+
+//static Team ReadPlayerCsvFile()
+//{
+//    const string PlayerCsvFile = "../../../Players.csv";
+//    Coach teamCoach = new Coach("Jay Woodcroft", DateTime.Parse("2022-02-10"));
+//    Team oilersTeam = new Team("Edmonton Oilers", teamCoach);
+//    try
+//    {
+//        string[] allLines= File.ReadAllLines(PlayerCsvFile);
+//        foreach(string line in allLines)
+//        {
+//            try
+//            {
+//                Player currentPlayer = null;
+//                bool success = Player.TryParse(line, out currentPlayer);
+//                if (success)
+//                {
+//                    oilersTeam.AddPlayer(currentPlayer);    
+//                }
+//            }
+//            catch(FormatException ex)
+//            {
+//                Console.WriteLine($"Format exception {ex.Message}");
+//            }
+//            catch(Exception ex)
+//            {
+//                Console.WriteLine($"Error parsing data from line {ex.Message}");
+//            }
+//        }
+//    }
+//    catch(Exception ex)
+//    {
+//        Console.WriteLine($"{ex.Message}");    
+//    }
+//    return oilersTeam;
+//}
+static Team ReadTeamInfoFromJsonFile()
+{
+    Team currentTeam = null!;
+    try
+    {
+        const string TeamJsonFile = "../../../Team.json";
+        string jsonString = File.ReadAllText(TeamJsonFile);
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+           WriteIndented = true,
+         // IncludeFields = true,
+        };
+        currentTeam = JsonSerializer.Deserialize<Team>(jsonString, options);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    return currentTeam;
+}
+static void DisplayTeamInfo(Team currentTeam)
+{
+    string name = currentTeam.TeamName;
+    string cName = currentTeam.Coach.FullName;
+    DateTime hireDate = currentTeam.Coach.StartDate;
+    List <Player> lPlayer = currentTeam.Players;
+    Console.WriteLine($"{name},{cName}, {hireDate}");
+    foreach (Player x in lPlayer)
+    {
+        Console.WriteLine($"{x.FullName}, {x.Position}, {x.PrimaryNo}, {x.Goals}, {x.Assists}, {x.Points}");
+    }
+    lPlayer.ForEach(player => Console.WriteLine(player.ToString()));
+}
+
+static void CreatePlayersCsvFile()
+{
+    DateTime startDate = DateTime.Parse("2021-09-02");
+    Coach oilersCoach = new Coach("Jay Woodcroft", startDate);
+    // Create a new Team
+    Team oilersTeam = new Team("Edmonton Oilers", oilersCoach);
+    // Add 3 players to the Team
+    Player player1 = new Player("Connor McDavid", Position.C, 97);
+    Player player2 = new Player("Evander Kane", Position.LW, 91);
+    Player player3 = new Player("Leeon Draisaitl", Position.C, 29);
+    oilersTeam.AddPlayer(player1);
+    oilersTeam.AddPlayer(player2);
+    oilersTeam.AddPlayer(player3);
+
+    const string PlayerCsvFile = "../../../Players.csv";
+    File.WriteAllLines(PlayerCsvFile,
+        oilersTeam.Players.Select(currentPlayer => currentPlayer.ToString()).ToList());
+
+}
+
+static void CreateTeamJsonFile()
+{
+    DateTime startDate = DateTime.Parse("2021-09-02");
+    Coach oilersCoach = new Coach("Jay Woodcroft", startDate);
+    // Create a new Team
+    Team oilersTeam = new Team("Edmonton Oilers", oilersCoach);
+    // Add 3 players to the Team
+    Player player1 = new Player("Connor McDavid", Position.C, 97);
+    Player player2 = new Player("Evander Kane", Position.LW, 91);
+    Player player3 = new Player("Leeon Draisaitl", Position.C, 29);
+    oilersTeam.AddPlayer(player1);
+    oilersTeam.AddPlayer(player2);
+    oilersTeam.AddPlayer(player3);
+
+    const string TeamJsonFile = "../../../Team.json";
+    JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        IncludeFields = true
+    };
+    string jsonString = JsonSerializer.Serialize<Team>(oilersTeam, options);
+    File.WriteAllText(TeamJsonFile, jsonString);
+
+
+}
